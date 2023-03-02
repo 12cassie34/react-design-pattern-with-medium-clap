13304cf93624c7c50d66bae0e934d3ef9cb18b30
Ines Lin <ineslin35@gmail.com>
2023-03-02 21:34:05 +0800

    feat: setup the dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { FunctionComponent } from "react";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 const App: FunctionComponent = () => {
   return (
-    <>
-
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 

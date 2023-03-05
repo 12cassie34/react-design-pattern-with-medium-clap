42874ab4c7908a47dcffc55829d93c90f575a25f
Ines Lin <ineslin35@gmail.com>
2023-03-05 17:52:50 +0800

    feat: change the layout's margin-top

--- src/pages/CustomHooks.tsx
+++ src/pages/CustomHooks.tsx
@@ -1,10 +1,7 @@
 import { FunctionComponent } from 'react';
 
-const CustomHooks: FunctionComponent = () => {
-
-    return (
-        <>
-            <h1>This is the page for custom hooks</h1>
-        </>
+const CustomHooks: FunctionComponent = () => (
+  <h1>This is the page for custom hooks</h1>
+);
 
 export default CustomHooks;

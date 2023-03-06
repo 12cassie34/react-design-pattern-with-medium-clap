9c37183654e79b147241fa95fa29355c390dcd50
Ines Lin <ineslin35@gmail.com>
2023-03-06 09:52:36 +0800

    chore: setting new linter tools

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 100,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: "all",
+  xmlWhitespaceSensitivity: "ignore",
+};
--- src/pages/CustomHooks.tsx
+++ src/pages/CustomHooks.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent } from 'react';
 
 const CustomHooks: FunctionComponent = () => (
-  <h1>This is the page for custom hooks</h1>
+    <h1>This is the page for custom hooks</h1>
 );
 
 export default CustomHooks;

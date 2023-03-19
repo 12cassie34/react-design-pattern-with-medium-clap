f5c68015960866ee99131e1909dceee23289a5d6
Cassie Lin <ineslin35@gmail.com>
2023-03-19 21:35:50 +0800

    feat: add the clap adding animation

--- src/components/MediumClap.tsx
+++ src/components/MediumClap.tsx
@@ -1,45 +1,81 @@
-import { FunctionComponent, useState } from "react";
 import { animated, useSpring } from "@react-spring/web";
+import React, { FunctionComponent, useState } from "react";
 
 import Chip from "@mui/material/Chip";
 import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import Typography from "@mui/material/Typography";
 
 const MediumClap: FunctionComponent = () => {
     const [isHover, setIsHover] = useState(false);
+    const [addedClap, setAddedClap] = useState(0);
+
     const boxShadowAnimation = useSpring({
-        from: {opacity: isHover ? 0 : 1, transition: 'ease' },
+        from: { opacity: isHover ? 0 : 1, transition: 'ease' },
         to: [{ opacity: isHover ? 1 : 0 }],
         loop: isHover,
         config: { duration: 1_000 },
-      });
+    });
+    const [springs, api] = useSpring(() => ({
+    }))
+
+    const handleAddedClap = () => {
+        if (addedClap < 50) {
+            api.start({
+                from: { opacity: 2, transform: 'translate(0px, 10px)', transition: 'ease-in-out', transitionDelay: '2500' },
+                to: { opacity: 0, transform: 'translate(0px, -100px)' },
+                config: { duration: 700 },
+            });
+            setAddedClap(addedClap + 1);
+        }
+    };
 
     return (
         <Paper sx={{ padding: '32px', width: '300px', backgroundColor: '#ffffff' }}>
-            <Chip label="The Original Clap 🎶" color="primary" sx={{ display: 'flex', marginBottom: '16px' }} />
-            <Stack sx={{ display: 'flex', alignItems: 'center' }}>
-                <div>
-                    <animated.div 
-                        style={{
-                            borderRadius: '50%',
-                            width: '60px',
-                            height:' 60px',
-                            position: 'absolute',
-                            boxShadow: '0px 0px 28px #90caf9',
-                            ...boxShadowAnimation,
-                        }}
-                    />
+            <Chip label="The Original Clap 🎶" color="primary" sx={{ display: 'flex', marginBottom: '60px' }} />
+            <Stack sx={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
+                <animated.div
+                    style={{
+                        position: 'relative',
+                        ...springs
+                    }}
+                >
+                    <IconButton sx={{
+                    padding: '20px',
+                    backgroundColor: '#90caf9',
+                    width: '20px',
+                    height: '20px',
+                }}>
+                        <Typography variant="body2">{`+${addedClap}`}</Typography>
+                    </IconButton>
+                </animated.div>
+                <Stack 
+                    onMouseEnter={() => setIsHover(true)}
+                    onMouseLeave={() => setIsHover(false)}
+                    sx={{
+                        position: 'absolute'
+                    }}
+                >
+                    <animated.div
+                    style={{
+                        borderRadius: '50%',
+                        width: '60px',
+                        height: ' 60px',
+                        position: 'absolute',
+                        boxShadow: '0px 0px 28px #90caf9',
+                        ...boxShadowAnimation,
+                    }}
+                />
                     <IconButton
-                        onMouseEnter={() => setIsHover(true)} 
-                        onMouseLeave={() => setIsHover(false)}
-                        aria-label="thumb up" 
-                        color="primary" 
-                        sx={{ border: '1px solid', width: '60px', height: '60px' }}>
+                    onClick={handleAddedClap}
+                    aria-label="thumb up"
+                    color="primary"
+                    sx={{ border: '1px solid', width: '60px', height: '60px', backgroundColor: '#ffffff' }}>
                         <ThumbUpIcon />
                     </IconButton>
-                </div>
+                </Stack>
             </Stack>
         </Paper>
     )
